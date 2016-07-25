882be8738b58771700c9bd35922e5dc70711760e
masihur <marufrahman1349@gmail.com>
2016-07-25 04:16:56 +0000

    association between user and article, enable debugger mode

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725011823) do
+ActiveRecord::Schema.define(version: 20160725025416) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "userid"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
