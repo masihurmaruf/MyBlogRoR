e166d47e548828a56a64a2d1d171946c371cf371
masihur <marufrahman1349@gmail.com>
2016-07-26 04:40:09 +0000

    add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show, :delete]
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
   
   def new
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
   end
   
   def show
-  
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
   def destroy
