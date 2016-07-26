f88c53fd548a6b79b97356f7de83f737afe92a8f
masihur <marufrahman1349@gmail.com>
2016-07-26 02:00:00 +0000

    add edit user page, update form partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show, :delete]
+  def index
+  
+  end
   
   def new
     @user = User.new
@@ -14,7 +18,31 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account is updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+  
+  end
+  
+  def destroy
+  
+  end
+  
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
