1f12a6724df1c203331bf3add286d5d68e2507bd
masihur <marufrahman1349@gmail.com>
2016-07-27 02:59:50 +0000

    update controller authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,11 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+  
+  def require_same_user
+    if !logged_in? || !current_user
+      flash[:danger] = "You must be logged in as the owner to perform this action"
+      redirect_to root_path
+    end
+  end
 end
