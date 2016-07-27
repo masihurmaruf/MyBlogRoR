46f4e5dbb969c310f055626ac2ed244e8b1be235
masihur <marufrahman1349@gmail.com>
2016-07-27 03:08:35 +0000

    add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id]
       flash[:success] = "Welcome #{@user.username}, to Masihur's Blog"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
