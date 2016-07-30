a13c203eddcbc8ca911fc1d9e22a8f4dc014a837
masihur <marufrahman1349@gmail.com>
2016-07-30 22:35:24 +0000

    add category show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,7 +22,7 @@ class CategoriesController < ApplicationController
   end
   
   def show
-    
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
   private 
